cf9b3b0564e81a4c3ed552dcec7adebdcbc0932b
zzj <zzj@qq.com>
2020-08-10 17:26:01 +0800

    处理异常

--- src/utils/request.js
+++ src/utils/request.js
@@ -34,10 +34,12 @@ service.interceptors.response.use(
     const res = response.data
     // 状态码判定
     if (res.code !== 200) {
-      Message.error(res.msg || 'Error', 5)
-      // 50008: 非法令牌; 50012: 其他客户端登录; 50014: 令牌过期;
-      if (res.code === 401 || res.code === 403) {
-        Modal.confirm('您已注销，您可以取消以停留在此页，或重新登录', '确认注销', {
+      if (res.code === 401) {
+        Message.error("没有访问权限", 5)
+      } else if (res.code === 403) {
+        Modal.confirm({
+          title: '确认注销',
+          content: '您已注销，您可以取消以停留在此页，或重新登录',
           okText: '重新登录',
           cancelText: '取消',
           type: 'warning'
@@ -46,6 +48,8 @@ service.interceptors.response.use(
             location.reload()
           })
         })
+      } else {
+        Message.error(res.msg || 'Error', 5)
       }
       return Promise.reject(new Error(res.msg || 'Error'))
     } else {
